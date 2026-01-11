b45adfc059cd560014ca629f874a9669f3b23cf3
spline <dmcemin@gmail.com>
2026-01-11 20:26:43 +0100

    add: person model import to models.__init__.py

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Base",
     "Genre",
     "Country",
+    "Person",
 )
 
 from .base import Base
 from .country import Country
 from .db_helper import db_helper
 from .genre import Genre
+from .person import Person
