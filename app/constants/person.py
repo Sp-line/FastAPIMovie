83c83ad8108d9f345619a4659f8f0caf76000aa4
spline <dmcemin@gmail.com>
2026-01-11 18:46:59 +0100

    add: person constants

---
+++ app/constants/person.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class MovieRoleType(enum.StrEnum):
+    ACTOR = "actor"
+    DIRECTOR = "director"
+    WRITER = "writer"
+    PRODUCER = "producer"
+
+
+PERSON_FULL_NAME_MAX_LEN = 150
+PERSON_SLUG_MAX_LEN = 150
