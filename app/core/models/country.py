21eee2e192f2950d4db74aab7f8dc962c69bb9b5
spline <dmcemin@gmail.com>
2026-01-11 19:29:08 +0100

    add: country model

---
+++ app/core/models/country.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from constants import COUNTRY_NAME_MAX_LEN, COUNTRY_SLUG_MAX_LEN
+from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class Country(IntIdPkMixin, Base):
+    name: Mapped[str] = mapped_column(String(COUNTRY_NAME_MAX_LEN), unique=True)
+    slug: Mapped[str] = mapped_column(String(COUNTRY_SLUG_MAX_LEN), unique=True)
