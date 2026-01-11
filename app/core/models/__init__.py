6c0025da929d1e19f2610726883cf412085615b2
spline <dmcemin@gmail.com>
2026-01-11 21:30:30 +0100

    add: movie model import to models.__init__.py

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -4,10 +4,12 @@ __all__ = (
     "Genre",
     "Country",
     "Person",
+    "Movie",
 )
 
 from .base import Base
 from .country import Country
 from .db_helper import db_helper
 from .genre import Genre
+from .movie import Movie
 from .person import Person
