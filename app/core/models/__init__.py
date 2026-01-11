5a6539b64b92d9801f2378aba13cc6537758af40
spline <dmcemin@gmail.com>
2026-01-11 19:29:43 +0100

    add: country model import to models.__init__.py

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "Genre",
+    "Country",
 )
 
 from .base import Base
+from .country import Country
 from .db_helper import db_helper
 from .genre import Genre
