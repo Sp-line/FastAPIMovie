fd8add5d8d42b0602f804f77e1e088296abf2439
spline <dmcemin@gmail.com>
2026-01-14 15:46:04 +0100

    add: MovieGenreAssociation model import to models.__init__.py

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -7,6 +7,7 @@ __all__ = (
     "Movie",
     "MovieShot",
     "MoviePersonAssociation",
+    "MovieGenreAssociation",
 )
 
 from .base import Base
@@ -14,6 +15,7 @@ from .country import Country
 from .db_helper import db_helper
 from .genre import Genre
 from .movie import Movie
+from .movie_genre_assoc import MovieGenreAssociation
 from .movie_person_assoc import MoviePersonAssociation
 from .movie_shot import MovieShot
 from .person import Person
