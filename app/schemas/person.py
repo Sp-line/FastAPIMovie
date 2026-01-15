d206eced5f141650c4f62ef762168052b5d227fb
spline <dmcemin@gmail.com>
2026-01-15 14:17:19 +0100

    add: person full_name, slug min & max len

--- app/schemas/person.py
+++ app/schemas/person.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from pydantic import BaseModel, Field, ConfigDict
 
 from constants import PERSON_FULL_NAME_MAX_LEN, PERSON_SLUG_MAX_LEN, IMAGE_URL_MAX_LEN
+from constants.person import PERSON_FULL_NAME_MIN_LEN, PERSON_SLUG_MIN_LEN
 
 
 class PersonBase(BaseModel):
-    full_name: Annotated[str, Field(max_length=PERSON_FULL_NAME_MAX_LEN)]
-    slug: Annotated[str, Field(max_length=PERSON_SLUG_MAX_LEN)]
+    full_name: Annotated[str, Field(min_length=PERSON_FULL_NAME_MIN_LEN, max_length=PERSON_FULL_NAME_MAX_LEN)]
+    slug: Annotated[str, Field(min_length=PERSON_SLUG_MIN_LEN, max_length=PERSON_SLUG_MAX_LEN)]
     photo_url: Annotated[str | None, Field(max_length=IMAGE_URL_MAX_LEN)] = None
 
 
@@ -16,8 +17,8 @@ class PersonCreate(PersonBase):
 
 
 class PersonUpdate(BaseModel):
-    full_name: Annotated[str | None, Field(max_length=PERSON_FULL_NAME_MAX_LEN)] = None
-    slug: Annotated[str | None, Field(max_length=PERSON_SLUG_MAX_LEN)] = None
+    full_name: Annotated[str | None, Field(min_length=PERSON_FULL_NAME_MIN_LEN, max_length=PERSON_FULL_NAME_MAX_LEN)] = None
+    slug: Annotated[str | None, Field(min_length=PERSON_SLUG_MIN_LEN, max_length=PERSON_SLUG_MAX_LEN)] = None
     photo_url: Annotated[str | None, Field(max_length=IMAGE_URL_MAX_LEN)] = None
 
 
