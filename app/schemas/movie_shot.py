8d686fb2505592c662349e23ed0207bb43109811
spline <dmcemin@gmail.com>
2026-01-15 13:25:12 +0100

    add: add movie_shot schemas

---
+++ app/schemas/movie_shot.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field, ConfigDict
+
+from constants import IMAGE_URL_MAX_LEN, MOVIE_SHOT_CAPTION_URL_MAX_LEN
+
+
+class MovieShotBase(BaseModel):
+    image_url: Annotated[str, Field(max_length=IMAGE_URL_MAX_LEN)]
+    caption: Annotated[str, Field(max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)]
+
+
+class MovieShotCreate(MovieShotBase):
+    movie_id: int
+
+
+class MovieShotUpdate(BaseModel):
+    image_url: Annotated[str | None, Field(max_length=IMAGE_URL_MAX_LEN)] = None
+    caption: Annotated[str | None, Field(max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)] = None
+
+
+class MovieShotRead(MovieShotBase):
+    id: int
+    movie_id: int
+
+    model_config = ConfigDict(from_attributes=True)
