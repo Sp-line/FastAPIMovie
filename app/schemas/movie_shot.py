3c33457208f132e1165e072f77f7f25670ce31e5
spline <dmcemin@gmail.com>
2026-01-15 14:08:29 +0100

    add: caption min len for movie_shot schemas

--- app/schemas/movie_shot.py
+++ app/schemas/movie_shot.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from pydantic import BaseModel, Field, ConfigDict
 
 from constants import IMAGE_URL_MAX_LEN, MOVIE_SHOT_CAPTION_URL_MAX_LEN
+from constants.movie_shot import MOVIE_SHOT_CAPTION_URL_MIN_LEN
 
 
 class MovieShotBase(BaseModel):
     image_url: Annotated[str, Field(max_length=IMAGE_URL_MAX_LEN)]
-    caption: Annotated[str, Field(max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)]
+    caption: Annotated[str, Field(min_length=MOVIE_SHOT_CAPTION_URL_MIN_LEN, max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)]
 
 
 class MovieShotCreate(MovieShotBase):
@@ -16,7 +17,7 @@ class MovieShotCreate(MovieShotBase):
 
 class MovieShotUpdate(BaseModel):
     image_url: Annotated[str | None, Field(max_length=IMAGE_URL_MAX_LEN)] = None
-    caption: Annotated[str | None, Field(max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)] = None
+    caption: Annotated[str | None, Field(min_length=MOVIE_SHOT_CAPTION_URL_MIN_LEN, max_length=MOVIE_SHOT_CAPTION_URL_MAX_LEN)] = None
 
 
 class MovieShotRead(MovieShotBase):
