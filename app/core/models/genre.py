7e8b5ed9a0aa9d55776810aa3d03ac23f6546413
spline <dmcemin@gmail.com>
2026-01-16 16:37:50 +0100

    add: CheckConstraints for Genre model

--- app/core/models/genre.py
+++ app/core/models/genre.py
@@ -1,9 +1,10 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import String, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from constants import GENRE_NAME_MAX_LEN, GENRE_SLUG_MAX_LEN
+from constants.genre import GENRE_NAME_MIN_LEN, GENRE_SLUG_MIN_LEN
 from core.models import Base
 from core.models.mixins.int_id_pk import IntIdPkMixin
 
@@ -12,6 +13,17 @@ if TYPE_CHECKING:
 
 
 class Genre(IntIdPkMixin, Base):
+    __table_args__ = (
+        CheckConstraint(
+            f"char_length(name) >= {GENRE_NAME_MIN_LEN}",
+            name="check_genre_name_min_len"
+        ),
+        CheckConstraint(
+            f"char_length(slug) >= {GENRE_SLUG_MIN_LEN}",
+            name="check_genre_slug_min_len"
+        ),
+    )
+
     name: Mapped[str] = mapped_column(String(GENRE_NAME_MAX_LEN), unique=True)
     slug: Mapped[str] = mapped_column(String(GENRE_SLUG_MAX_LEN), unique=True)
 
