c43c4a0c8e07fd156f037d9c53e086f47d40241a
spline <dmcemin@gmail.com>
2026-01-16 16:32:00 +0100

    add: CheckConstraints for Country model

--- app/core/models/country.py
+++ app/core/models/country.py
@@ -1,9 +1,10 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import String, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from constants import COUNTRY_NAME_MAX_LEN, COUNTRY_SLUG_MAX_LEN
+from constants.country import COUNTRY_NAME_MIN_LEN, COUNTRY_SLUG_MIN_LEN
 from core.models import Base
 from core.models.mixins.int_id_pk import IntIdPkMixin
 
@@ -13,6 +14,16 @@ if TYPE_CHECKING:
 
 class Country(IntIdPkMixin, Base):
     __tablename__ = "countries"
+    __table_args__ = (
+        CheckConstraint(
+            f"char_length(name) >= {COUNTRY_NAME_MIN_LEN}",
+            name="check_country_name_min_len"
+        ),
+        CheckConstraint(
+            f"char_length(slug) >= {COUNTRY_SLUG_MIN_LEN}",
+            name="check_country_slug_min_len"
+        ),
+    )
 
     name: Mapped[str] = mapped_column(String(COUNTRY_NAME_MAX_LEN), unique=True)
     slug: Mapped[str] = mapped_column(String(COUNTRY_SLUG_MAX_LEN), unique=True)
