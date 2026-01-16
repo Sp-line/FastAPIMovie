1ab3f95e9a90dc1b4449996bbc4a9d3b3be55a43
spline <dmcemin@gmail.com>
2026-01-16 16:46:21 +0100

    add: CheckConstraints for movie_shot

--- app/core/models/movie_shot.py
+++ app/core/models/movie_shot.py
@@ -1,9 +1,11 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from constants import MOVIE_SHOT_CAPTION_URL_MAX_LEN, IMAGE_URL_MAX_LEN
+from constants.base import IMAGE_URL_MIN_LEN
+from constants.movie_shot import MOVIE_SHOT_CAPTION_URL_MIN_LEN
 from core.models import Base
 from core.models.mixins.int_id_pk import IntIdPkMixin
 
@@ -12,6 +14,17 @@ if TYPE_CHECKING:
 
 
 class MovieShot(IntIdPkMixin, Base):
+    __table_args__ = (
+        CheckConstraint(
+            f"char_length(caption) >= {MOVIE_SHOT_CAPTION_URL_MIN_LEN}",
+            name="check_movie_shot_caption_min_len"
+        ),
+        CheckConstraint(
+            f"char_length(image_url) > {IMAGE_URL_MIN_LEN}",
+            name="check_movie_shot_image_url_not_empty"
+        ),
+    )
+
     image_url: Mapped[str] = mapped_column(String(IMAGE_URL_MAX_LEN))
     caption: Mapped[str] = mapped_column(String(MOVIE_SHOT_CAPTION_URL_MAX_LEN))
 
