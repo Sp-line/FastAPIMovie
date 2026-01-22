de1108fefe7abc05f4fba8d21dd7846fb4ade933
spline <dmcemin@gmail.com>
2026-01-23 00:31:16 +0100

    add: routers to api_v1.__init__.py

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,7 +1,24 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .country import router as country_router
+from .genres import router as genres_router
+from .movie import router as movie_router
+from .movie_country import router as movie_country_router
+from .movie_genre import router as movie_genre_router
+from .movie_person import router as movie_person_router
+from .movie_shot import router as movie_shot_router
+from .person import router as person_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+
+router.include_router(country_router, prefix="/countries", tags=["Countries"])
+router.include_router(genres_router, prefix="/genres", tags=["Genres"])
+router.include_router(movie_router, prefix="/movies", tags=["Movies"])
+router.include_router(person_router, prefix="/persons", tags=["Persons"])
+router.include_router(movie_shot_router, prefix="/movie-shots", tags=["Movie-Shots"])
+router.include_router(movie_country_router, tags=["Movie-Country-Associations"])
+router.include_router(movie_genre_router, tags=["Movie-Genre-Associations"])
+router.include_router(movie_person_router, tags=["Movie-Person-Associations"])
