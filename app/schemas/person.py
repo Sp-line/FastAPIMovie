5ccee7b7af6ef50fa298fa1c39e35f0df1b247d9
spline <dmcemin@gmail.com>
2026-01-22 12:24:44 +0100

    change: person schemas

--- app/schemas/person.py
+++ app/schemas/person.py
@@ -6,20 +6,30 @@ from constants import ImageUrlLimits, PersonLimits
 
 
 class PersonBase(BaseModel):
-    full_name: Annotated[str, Field(min_length=PersonLimits.FULL_NAME_MIN, max_length=PersonLimits.FULL_NAME_MIN)]
+    full_name: Annotated[str, Field(min_length=PersonLimits.FULL_NAME_MIN, max_length=PersonLimits.FULL_NAME_MAX)]
     slug: Annotated[str, Field(min_length=PersonLimits.SLUG_MIN, max_length=PersonLimits.SLUG_MAX)]
     photo_url: Annotated[str | None, Field(min_length=ImageUrlLimits.MIN, max_length=ImageUrlLimits.MAX)] = None
 
 
-class PersonCreate(PersonBase):
+class PersonCreateDB(PersonBase):
     pass
 
 
-class PersonUpdate(BaseModel):
+class PersonCreateReq(BaseModel):
+    full_name: Annotated[str, Field(min_length=PersonLimits.FULL_NAME_MIN, max_length=PersonLimits.FULL_NAME_MAX)]
+
+
+class PersonUpdateBase(BaseModel):
     full_name: Annotated[
-        str | None, Field(min_length=PersonLimits.FULL_NAME_MIN, max_length=PersonLimits.FULL_NAME_MIN)] = None
-    slug: Annotated[str | None, Field(min_length=PersonLimits.SLUG_MIN, max_length=PersonLimits.SLUG_MAX)] = None
+        str | None, Field(min_length=PersonLimits.FULL_NAME_MIN, max_length=PersonLimits.FULL_NAME_MAX)] = None
+
+
+class PersonUpdateDB(PersonUpdateBase):
     photo_url: Annotated[str | None, Field(min_length=ImageUrlLimits.MIN, max_length=ImageUrlLimits.MAX)] = None
+
+
+class PersonUpdateReq(PersonUpdateBase):
+    pass
 
 
 class PersonRead(PersonBase):
