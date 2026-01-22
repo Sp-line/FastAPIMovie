d096492100111be6d40990246a3b30163d762322
spline <dmcemin@gmail.com>
2026-01-23 00:05:03 +0100

    add: MovieShotFileService

--- app/services/movie_shot.py
+++ app/services/movie_shot.py
@@ -3,6 +3,8 @@ from repositories.unit_of_work import UnitOfWork
 from schemas.movie_shot import MovieShotRead, MovieShotCreateReq, MovieShotCreateDB, MovieShotUpdateDB, \
     MovieShotUpdateReq
 from services.base import ServiceBase
+from services.file import FileService
+from services.s3 import S3Service
 
 
 class MovieShotService(ServiceBase[MovieShotRepository, MovieShotRead, MovieShotCreateReq, MovieShotUpdateReq]):
@@ -23,3 +25,23 @@ class MovieShotService(ServiceBase[MovieShotRepository, MovieShotRead, MovieShot
 
     def _prepare_create_data(self, data: MovieShotCreateReq) -> MovieShotCreateDB:
         return MovieShotCreateDB(**data.model_dump())
+
+
+class MovieShotFileService(FileService[MovieShotRead]):
+    def __init__(
+            self,
+            s3_service: S3Service,
+            repository: MovieShotRepository,
+            unit_of_work: UnitOfWork
+    ):
+        super().__init__(
+            file_service=s3_service,
+            repository=repository,
+            unit_of_work=unit_of_work,
+            read_schema_type=MovieShotRead,
+            update_schema_type=MovieShotUpdateDB,
+            table_name="movie_shots",
+            folder="movie_shots/images",
+            url_field="image_url",
+            filename_field="caption"
+        )
