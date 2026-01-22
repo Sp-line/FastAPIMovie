c203168ce28ce18d3dfd49275f545a13887ef5e7
spline <dmcemin@gmail.com>
2026-01-23 00:16:48 +0100

    add: MovieService

---
+++ app/services/movie.py
@@ -0,0 +1,40 @@
+from slugify import slugify
+
+from exceptions.db import ObjectNotFoundException
+from repositories.movie import MovieRepository
+from repositories.unit_of_work import UnitOfWork
+from schemas.movie import MovieRead, MovieList, MovieCreateReq, MovieCreateDB, MovieUpdateDB, MovieUpdateReq, \
+    MovieDetail
+from services.base import ServiceBase
+
+
+class MovieService(ServiceBase[MovieRepository, MovieRead, MovieCreateReq, MovieUpdateReq]):
+    def __init__(
+            self,
+            repository: MovieRepository,
+            unit_of_work: UnitOfWork,
+    ) -> None:
+        super().__init__(
+            repository=repository,
+            unit_of_work=unit_of_work,
+            table_name="movies",
+            read_schema_type=MovieRead
+        )
+
+    def _prepare_update_data(self, data: MovieUpdateReq) -> MovieUpdateDB:
+        return MovieUpdateDB(**data.model_dump(exclude_unset=True))
+
+    def _prepare_create_data(self, data: MovieCreateReq) -> MovieCreateDB:
+        return MovieCreateDB(
+            **data.model_dump(),
+            slug=slugify(data.title)
+        )
+
+    async def get_all(self) -> list[MovieList]:
+        items = await self._repository.get_for_list()
+        return [MovieList.model_validate(movie) for movie in items]
+
+    async def get_by_id(self, movie_id: int) -> MovieDetail:
+        if not (movie := await self._repository.get_for_read(movie_id)):
+            raise ObjectNotFoundException(movie_id, self._table_name)
+        return MovieDetail.model_validate(movie)
