73805131fe727d846a183ea5450c649eef85fb57
spline <dmcemin@gmail.com>
2026-01-23 00:17:52 +0100

    add: MovieFileService

--- app/services/movie.py
+++ app/services/movie.py
@@ -6,6 +6,8 @@ from repositories.unit_of_work import UnitOfWork
 from schemas.movie import MovieRead, MovieList, MovieCreateReq, MovieCreateDB, MovieUpdateDB, MovieUpdateReq, \
     MovieDetail
 from services.base import ServiceBase
+from services.file import FileService
+from services.s3 import S3Service
 
 
 class MovieService(ServiceBase[MovieRepository, MovieRead, MovieCreateReq, MovieUpdateReq]):
@@ -38,3 +40,23 @@ class MovieService(ServiceBase[MovieRepository, MovieRead, MovieCreateReq, Movie
         if not (movie := await self._repository.get_for_read(movie_id)):
             raise ObjectNotFoundException(movie_id, self._table_name)
         return MovieDetail.model_validate(movie)
+
+
+class MovieFileService(FileService[MovieRead]):
+    def __init__(
+            self,
+            s3_service: S3Service,
+            repository: MovieRepository,
+            unit_of_work: UnitOfWork
+    ):
+        super().__init__(
+            file_service=s3_service,
+            repository=repository,
+            unit_of_work=unit_of_work,
+            read_schema_type=MovieRead,
+            update_schema_type=MovieUpdateDB,
+            table_name="movies",
+            folder="movies/posters",
+            url_field="poster_url",
+            filename_field="slug"
+        )
