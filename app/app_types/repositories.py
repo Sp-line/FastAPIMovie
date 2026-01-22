558d0c9fee8d5a0dcb128cc4ebb6d12826c7b835
spline <dmcemin@gmail.com>
2026-01-22 23:22:32 +0100

    add: M2MRepositoryBaseType TypeVar

--- app/app_types/repositories.py
+++ app/app_types/repositories.py
@@ -1,5 +1,6 @@
 from typing import TypeVar
 
-from repositories.base import RepositoryBase
+from repositories.base import RepositoryBase, M2MRepositoryBase
 
 RepositoryBaseType = TypeVar("RepositoryBaseType", bound=RepositoryBase)
+M2MRepositoryBaseType = TypeVar("M2MRepositoryBaseType", bound=M2MRepositoryBase)
