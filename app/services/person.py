35f15f69d8e3faea7bd934872829c25ed29ac6c1
spline <dmcemin@gmail.com>
2026-01-23 00:03:22 +0100

    add: PersonFileService

--- app/services/person.py
+++ app/services/person.py
@@ -4,6 +4,8 @@ from repositories.person import PersonRepository
 from repositories.unit_of_work import UnitOfWork
 from schemas.person import PersonRead, PersonCreateDB, PersonUpdateDB, PersonCreateReq, PersonUpdateReq
 from services.base import ServiceBase
+from services.file import FileService
+from services.s3 import S3Service
 
 
 class PersonService(ServiceBase[PersonRepository, PersonRead, PersonCreateReq, PersonUpdateReq]):
@@ -26,4 +28,24 @@ class PersonService(ServiceBase[PersonRepository, PersonRead, PersonCreateReq, P
         return PersonCreateDB(
             **data.model_dump(),
             slug=slugify(data.full_name)
+        )
+
+
+class PersonFileService(FileService[PersonRead]):
+    def __init__(
+            self,
+            s3_service: S3Service,
+            repository: PersonRepository,
+            unit_of_work: UnitOfWork
+    ):
+        super().__init__(
+            file_service=s3_service,
+            repository=repository,
+            unit_of_work=unit_of_work,
+            read_schema_type=PersonRead,
+            update_schema_type=PersonUpdateDB,
+            table_name="persons",
+            folder="persons/photos",
+            url_field="photo_url",
+            filename_field="slug"
         )
