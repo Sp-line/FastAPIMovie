83b9495f907c709fd83ad23dfcfe69e4b1e7a152
spline <dmcemin@gmail.com>
2026-01-22 12:22:05 +0100

    change: genre schemas

--- app/schemas/genre.py
+++ app/schemas/genre.py
@@ -10,16 +10,27 @@ class GenreBase(BaseModel):
     slug: Annotated[str, Field(min_length=GenreLimits.SLUG_MIN, max_length=GenreLimits.SLUG_MAX)]
 
 
-class GenreCreate(GenreBase):
+class GenreCreateDB(GenreBase):
     pass
 
 
-class GenreUpdate(BaseModel):
-    name: Annotated[str | None, Field(min_length=GenreLimits.NAME_MIN, max_length=GenreLimits.NAME_MAX)] = None
-    slug: Annotated[str | None, Field(min_length=GenreLimits.SLUG_MIN, max_length=GenreLimits.SLUG_MAX)] = None
+class GenreCreateReq(BaseModel):
+    name: Annotated[str, Field(min_length=GenreLimits.NAME_MIN, max_length=GenreLimits.NAME_MAX)]
 
 
 class GenreRead(GenreBase):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class GenreUpdateBase(BaseModel):
+    name: Annotated[str | None, Field(min_length=GenreLimits.NAME_MIN, max_length=GenreLimits.NAME_MAX)] = None
+
+
+class GenreUpdateDB(GenreUpdateBase):
+    slug: Annotated[str | None, Field(min_length=GenreLimits.SLUG_MIN, max_length=GenreLimits.SLUG_MAX)] = None
+
+
+class GenreUpdateReq(GenreUpdateBase):
+    pass
