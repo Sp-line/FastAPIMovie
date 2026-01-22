3909bfbcbc026c91934fe0cc4e4ed65c16e61a7f
spline <dmcemin@gmail.com>
2026-01-22 13:05:21 +0100

    add: ReadSchemaType & CompositeIdSchemaType

--- app/app_types/schemas.py
+++ app/app_types/schemas.py
@@ -4,3 +4,5 @@ from pydantic import BaseModel
 
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+ReadSchemaType = TypeVar("ReadSchemaType", bound=BaseModel)
+CompositeIdSchemaType = TypeVar("CompositeIdSchemaType", bound=BaseModel)
