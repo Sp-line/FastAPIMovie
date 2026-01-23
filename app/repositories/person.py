5be75a099def3c03fb50414177cb623ecfdfaa6f
spline <dmcemin@gmail.com>
2026-01-24 00:18:51 +0100

    fix: PersonRepository _handle_integrity_error

--- app/repositories/person.py
+++ app/repositories/person.py
@@ -1,4 +1,3 @@
-from asyncpg import exceptions as pg_exc
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,16 +12,17 @@ class PersonRepository(RepositoryBase[Person, PersonCreateDB, PersonUpdateDB]):
         super().__init__(Person, session)
 
     def _handle_integrity_error(self, exc: IntegrityError) -> None:
-        orig = exc.orig
+        err_data = self._get_integrity_error_data(exc)
 
-        if isinstance(orig, pg_exc.UniqueViolationError):
-            match getattr(exc.orig, "constraint_name", None):
-                case "uq_persons_slug":
-                    raise UniqueFieldException(field_name="slug", table_name="persons")
-        elif isinstance(orig, pg_exc.ForeignKeyViolationError):
-            match getattr(orig, "constraint_name", None):
-                case "fk_movie_person_associations_person_id_persons":
-                    raise DeleteConstraintException(
-                        table_name="persons",
-                        referencing_table="movies"
-                    )
+        match err_data.sqlstate:
+            case "23505":
+                match err_data.constraint_name:
+                    case "uq_persons_slug":
+                        raise UniqueFieldException(field_name="slug", table_name=err_data.table_name)
+            case "23001":
+                match err_data.constraint_name:
+                    case "fk_movie_person_associations_person_id_persons":
+                        raise DeleteConstraintException(
+                            table_name=err_data.table_name,
+                            referencing_table="movies"
+                        )
