a986055ab284e25b19e3ed76a89c6228a73265bd
spline <dmcemin@gmail.com>
2026-01-25 00:13:26 +0100

    delete: unused type vars

--- app/app_types/models.py
+++
@@ -1,5 +0,0 @@
-from typing import TypeVar
-
-from core.models.mixins.int_id_pk import IntIdPkMixin
-
-ModelType = TypeVar("ModelType", bound=IntIdPkMixin)
--- app/app_types/repositories.py
+++
@@ -1,6 +0,0 @@
-from typing import TypeVar
-
-from repositories.base import RepositoryBase, M2MRepositoryBase
-
-RepositoryBaseType = TypeVar("RepositoryBaseType", bound=RepositoryBase)
-M2MRepositoryBaseType = TypeVar("M2MRepositoryBaseType", bound=M2MRepositoryBase)
