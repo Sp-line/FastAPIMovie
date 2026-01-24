90e82717779492a6d610755a693e4ff124dc803b
spline <dmcemin@gmail.com>
2026-01-25 00:17:25 +0100

    delete: app_types package

--- app/app_types/schemas.py
+++
@@ -1,8 +0,0 @@
-from typing import TypeVar
-
-from pydantic import BaseModel
-
-CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
-UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
-ReadSchemaType = TypeVar("ReadSchemaType", bound=BaseModel)
-CompositeIdSchemaType = TypeVar("CompositeIdSchemaType", bound=BaseModel)
