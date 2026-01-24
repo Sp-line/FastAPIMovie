26c233ce4e95b08dae1a11831378e6a00fb7965a
spline <dmcemin@gmail.com>
2026-01-24 11:45:07 +0100

    add: register_exception_handlers

---
+++ app/exceptions/register.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from exceptions.db_handlers import register_db_exception_handlers
+from exceptions.s3_handlers import register_s3_exception_handlers
+
+
+def register_exception_handlers(app: FastAPI) -> None:
+    register_db_exception_handlers(app)
+    register_s3_exception_handlers(app)
