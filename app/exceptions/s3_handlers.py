30c7562bb6ef92a605222abf7777f6e9dffff993
spline <dmcemin@gmail.com>
2026-01-24 11:44:35 +0100

    add: s3_handlers

---
+++ app/exceptions/s3_handlers.py
@@ -0,0 +1,33 @@
+from fastapi import Request, status, FastAPI
+from fastapi.responses import ORJSONResponse
+
+from exceptions.s3 import S3ClientNotInitializedException, S3UploadException, S3DeleteException
+
+
+def register_s3_exception_handlers(app: FastAPI) -> None:
+    @app.exception_handler(S3ClientNotInitializedException)
+    async def s3_client_config_handler(request: Request, exc: S3ClientNotInitializedException):
+        return ORJSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": str(exc)},
+        )
+
+    @app.exception_handler(S3UploadException)
+    async def s3_upload_handler(request: Request, exc: S3UploadException):
+        return ORJSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "object_name": exc.obj_name,
+                "detail": str(exc)
+            },
+        )
+
+    @app.exception_handler(S3DeleteException)
+    async def s3_delete_handler(request: Request, exc: S3DeleteException):
+        return ORJSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "object_name": exc.obj_name,
+                "detail": str(exc)
+            },
+        )
