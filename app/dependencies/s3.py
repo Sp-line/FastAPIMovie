32ce2dda7734cbcf53ba0cf51f3d2850c6ad7a59
spline <dmcemin@gmail.com>
2026-01-25 20:47:19 +0100

    add: S3ClientDep & S3ServiceDep types

--- app/dependencies/s3.py
+++ app/dependencies/s3.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, TypeAlias
 
 from fastapi import Depends
 from types_aiobotocore_s3.client import S3Client
@@ -7,6 +7,11 @@ from core.config import settings
 from services.s3 import S3Service
 from storage.s3 import s3_helper as s3
 
+S3ClientDep: TypeAlias = Annotated[S3Client, Depends(s3.get_client)]
 
-def get_s3_service(client: Annotated[S3Client, Depends(s3.get_client)]) -> S3Service:
+
+def get_s3_service(client: S3ClientDep) -> S3Service:
     return S3Service(client=client, bucket_name=settings.s3.bucket_name)
+
+
+S3ServiceDep: TypeAlias = Annotated[S3Service, Depends(get_s3_service)]
