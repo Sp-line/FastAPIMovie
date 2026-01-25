3eaee5859f31c8732bef044d5d983fe7dd9bfbeb
spline <dmcemin@gmail.com>
2026-01-25 20:45:56 +0100

    add: AsyncSessionDep & UnitOfWorkDep types

--- app/dependencies/db.py
+++ app/dependencies/db.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, TypeAlias
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,6 +6,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper as db
 from repositories.unit_of_work import UnitOfWork
 
+AsyncSessionDep: TypeAlias = Annotated[AsyncSession, Depends(db.session_getter)]
 
-def get_uow(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> UnitOfWork:
+
+def get_uow(session: AsyncSessionDep) -> UnitOfWork:
     return UnitOfWork(session)
+
+
+UnitOfWorkDep: TypeAlias = Annotated[UnitOfWork, Depends(get_uow)]
