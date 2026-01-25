336ae77db30601368daeb1aa4cda1a0484a43f87
spline <dmcemin@gmail.com>
2026-01-25 20:46:56 +0100

    add: RepositoryDep types

--- app/dependencies/repositories.py
+++ app/dependencies/repositories.py
@@ -1,8 +1,8 @@
-from typing import Annotated
+from typing import TypeAlias, Annotated
 
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from core.models import db_helper as db
+
+from dependencies.db import AsyncSessionDep
 from repositories.country import CountryRepository
 from repositories.genre import GenreRepository
 from repositories.m2m import MovieCountryRepository, MovieGenreRepository, MoviePersonRepository
@@ -11,33 +11,43 @@ from repositories.movie_shot import MovieShotRepository
 from repositories.person import PersonRepository
 
 
-def get_movie_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> MovieRepository:
+def get_movie_repository(session: AsyncSessionDep) -> MovieRepository:
     return MovieRepository(session)
 
 
-def get_person_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> PersonRepository:
+def get_person_repository(session: AsyncSessionDep) -> PersonRepository:
     return PersonRepository(session)
 
 
-def get_movie_shot_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> MovieShotRepository:
+def get_movie_shot_repository(session: AsyncSessionDep) -> MovieShotRepository:
     return MovieShotRepository(session)
 
 
-def get_genre_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> GenreRepository:
+def get_genre_repository(session: AsyncSessionDep) -> GenreRepository:
     return GenreRepository(session)
 
 
-def get_country_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> CountryRepository:
+def get_country_repository(session: AsyncSessionDep) -> CountryRepository:
     return CountryRepository(session)
 
 
-def get_movie_country_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> MovieCountryRepository:
+def get_movie_country_repository(session: AsyncSessionDep) -> MovieCountryRepository:
     return MovieCountryRepository(session)
 
 
-def get_movie_genre_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> MovieGenreRepository:
+def get_movie_genre_repository(session: AsyncSessionDep) -> MovieGenreRepository:
     return MovieGenreRepository(session)
 
 
-def get_movie_person_repository(session: Annotated[AsyncSession, Depends(db.session_getter)]) -> MoviePersonRepository:
+def get_movie_person_repository(session: AsyncSessionDep) -> MoviePersonRepository:
     return MoviePersonRepository(session)
+
+
+MovieRepositoryDep: TypeAlias = Annotated[MovieRepository, Depends(get_movie_repository)]
+MovieCountryRepositoryDep: TypeAlias = Annotated[MovieCountryRepository, Depends(get_movie_country_repository)]
+MovieGenreRepositoryDep: TypeAlias = Annotated[MovieGenreRepository, Depends(get_movie_genre_repository)]
+MoviePersonRepositoryDep: TypeAlias = Annotated[MoviePersonRepository, Depends(get_movie_person_repository)]
+PersonRepositoryDep: TypeAlias = Annotated[PersonRepository, Depends(get_person_repository)]
+MovieShotRepositoryDep: TypeAlias = Annotated[MovieShotRepository, Depends(get_movie_shot_repository)]
+CountryRepositoryDep: TypeAlias = Annotated[CountryRepository, Depends(get_country_repository)]
+GenreRepositoryDep: TypeAlias = Annotated[GenreRepository, Depends(get_genre_repository)]
