4bf26c12acd6cc3e4c42624df5c36e45ef2bf9d4
spline <dmcemin@gmail.com>
2026-01-26 23:03:35 +0100

    use: S3ClientConfig for S3Helper

--- app/storage/s3.py
+++ app/storage/s3.py
@@ -1,33 +1,21 @@
-from typing import Any
-
 import aioboto3
 from types_aiobotocore_s3 import S3Client
 
 from core.config import settings
 from exceptions.s3 import S3ClientNotInitializedException
+from schemas.s3_client_config import S3ClientConfig
 
 
 class S3Helper:
-    def __init__(
-            self,
-            endpoint_url: str,
-            access_key: str,
-            secret_key: str,
-            region: str = "us-east-1",
-    ) -> None:
-        self._config: dict[str, Any] = {
-            "endpoint_url": endpoint_url,
-            "aws_access_key_id": access_key,
-            "aws_secret_access_key": secret_key,
-            "region_name": region,
-        }
+    def __init__(self, config: S3ClientConfig) -> None:
+        self._config = config
         self.session: aioboto3.Session = aioboto3.Session()
         self._client: S3Client | None = None
 
     async def connect(self) -> None:
         if self._client is not None:
             return
-        self._client = await self.session.client("s3", **self._config).__aenter__()
+        self._client = await self.session.client("s3", **self._config.model_dump()).__aenter__()
 
     async def close(self) -> None:
         if self._client is None:
@@ -42,8 +30,10 @@ class S3Helper:
 
 
 s3_helper = S3Helper(
-    endpoint_url=str(settings.s3.endpoint_url),
-    access_key=settings.s3.access_key,
-    secret_key=settings.s3.secret_key,
-    region=settings.s3.region,
+    S3ClientConfig(
+        endpoint_url=str(settings.s3.endpoint_url),
+        aws_access_key_id=settings.s3.access_key,
+        aws_secret_access_key=settings.s3.secret_key,
+        region_name=settings.s3.region
+    )
 )
