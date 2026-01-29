e0d861f3af74a2d8293c996fbbf460d3d0d78a8f
spline <dmcemin@gmail.com>
2026-01-29 14:11:52 +0100

    add: checking for hasattr(self, "_set_handler") for GunicornLogger.setup

--- app/core/gunicorn/logger.py
+++ app/core/gunicorn/logger.py
@@ -9,13 +9,14 @@ class GunicornLogger(Logger):
     def setup(self, cfg) -> None:
         super().setup(cfg)
 
-        self._set_handler(
-            log=self.access_log,
-            output=cfg.accesslog,
-            fmt=Formatter(fmt=settings.logging.log_format)
-        )
-        self._set_handler(
-            log=self.error_log,
-            output=cfg.errorlog,
-            fmt=Formatter(fmt=settings.logging.log_format)
-        )
+        if hasattr(self, "_set_handler"):
+            self._set_handler(
+                log=self.access_log,
+                output=cfg.accesslog,
+                fmt=Formatter(fmt=settings.logging.log_format)
+            )
+            self._set_handler(
+                log=self.error_log,
+                output=cfg.errorlog,
+                fmt=Formatter(fmt=settings.logging.log_format)
+            )
