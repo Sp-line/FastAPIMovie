d0048adeacf68bcac4286b9b9a346f2d6ba4d28b
spline <dmcemin@gmail.com>
2026-01-29 14:14:17 +0100

    add: TReadSchema bound type for SlugFilePathBuilder

--- app/storage/path_builder.py
+++ app/storage/path_builder.py
@@ -1,10 +1,11 @@
+from pydantic import BaseModel
 from slugify import slugify
 
 from exceptions.s3 import FilePathBuilderNoneValueException
 from storage.abc import FilePathBuilderABC
 
 
-class SlugFilePathBuilder[TReadSchema](FilePathBuilderABC[TReadSchema]):
+class SlugFilePathBuilder[TReadSchema: BaseModel](FilePathBuilderABC[TReadSchema]):
     def __init__(self, folder: str, field: str):
         self._folder = folder
         self._field = field
