ce6d4b01ba0f5c319ede9bea6ff82ac5104d23ae
spline <dmcemin@gmail.com>
2026-02-02 16:20:20 +0100

    add: CountryCacheInvalidatorDep

--- app/dependencies/cache.py
+++ app/dependencies/cache.py
@@ -4,5 +4,14 @@ from fastapi import Depends
 from redis.asyncio.client import Redis
 
 from cache import redis_helper
+from cache.invalidator import CacheInvalidatorBase
+from schemas.cache import ModelCacheConfig
 
 RedisDep: TypeAlias = Annotated[Redis, Depends(redis_helper.get_client)]
+
+
+def get_countries_cache_invalidator(cache: RedisDep) -> CacheInvalidatorBase:
+    return CacheInvalidatorBase(cache, ModelCacheConfig(), "countries")
+
+
+CountryCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_countries_cache_invalidator)]
