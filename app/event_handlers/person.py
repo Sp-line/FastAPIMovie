562d71afc9633ce066f57ae98f4c7f7f16a9226e
spline <dmcemin@gmail.com>
2026-02-02 22:47:56 +0100

    fix: use persons for cache key handlers name

--- app/event_handlers/person.py
+++ app/event_handlers/person.py
@@ -5,7 +5,7 @@ from schemas.person import PersonCreateEvent, PersonUpdateEvent
 
 
 @fs_router.subscriber("persons.created")
-async def persons_created_invalidate_countries_list_cache(
+async def persons_created_invalidate_persons_list_cache(
         payload: PersonCreateEvent,
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
@@ -13,7 +13,7 @@ async def persons_created_invalidate_countries_list_cache(
 
 
 @fs_router.subscriber("persons.bulk.created")
-async def persons_bulk_created_invalidate_countries_list_cache(
+async def persons_bulk_created_invalidate_persons_list_cache(
         payload: list[PersonCreateEvent],
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
@@ -21,7 +21,7 @@ async def persons_bulk_created_invalidate_countries_list_cache(
 
 
 @fs_router.subscriber("persons.updated")
-async def persons_updated_invalidate_countries_list_cache(
+async def persons_updated_invalidate_persons_list_cache(
         payload: PersonUpdateEvent,
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
@@ -29,7 +29,7 @@ async def persons_updated_invalidate_countries_list_cache(
 
 
 @fs_router.subscriber("persons.updated")
-async def persons_updated_invalidate_countries_retrieve_cache(
+async def persons_updated_invalidate_persons_retrieve_cache(
         payload: PersonUpdateEvent,
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
@@ -37,7 +37,7 @@ async def persons_updated_invalidate_countries_retrieve_cache(
 
 
 @fs_router.subscriber("persons.deleted")
-async def persons_deleted_invalidate_countries_list_cache(
+async def persons_deleted_invalidate_persons_list_cache(
         payload: Id,
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
@@ -45,7 +45,7 @@ async def persons_deleted_invalidate_countries_list_cache(
 
 
 @fs_router.subscriber("persons.deleted")
-async def persons_deleted_invalidate_countries_retrieve_cache(
+async def persons_deleted_invalidate_persons_retrieve_cache(
         payload: Id,
         cache_invalidator: PersonCacheInvalidatorDep
 ) -> None:
