c08eb831833f9f6216094c2c03d5aacca0d2d163
spline <dmcemin@gmail.com>
2026-02-03 23:21:32 +0100

    delete: unused movie_genre endpoints

--- app/api/api_v1/movie_genre.py
+++ app/api/api_v1/movie_genre.py
@@ -6,23 +6,6 @@ from schemas.movie_genre import MovieGenreRead, MovieGenreCreate, MovieGenreUpda
 router = APIRouter()
 
 
-@router.get("/")
-async def get_movie_genre_associations(
-        service: MovieGenreServiceDep,
-        skip: int = 0,
-        limit: int = 100
-) -> list[MovieGenreRead]:
-    return await service.get_all(skip, limit)
-
-
-@router.get("/{movie_genre_association_id}")
-async def get_movie_genre_association(
-        movie_genre_association_id: int,
-        service: MovieGenreServiceDep
-) -> MovieGenreRead:
-    return await service.get_by_id(movie_genre_association_id)
-
-
 @router.post("/")
 async def create_movie_genre_association(
         data: MovieGenreCreate,
@@ -37,18 +20,6 @@ async def bulk_create_movie_genre_associations(
         service: MovieGenreServiceDep
 ) -> list[MovieGenreRead]:
     return await service.bulk_create(data)
-
-
-@router.patch("/{movie_genre_association_id}")
-async def update_movie_genre_association(
-        movie_genre_association_id: int,
-        data: MovieGenreUpdate,
-        service: MovieGenreServiceDep
-) -> MovieGenreRead:
-    return await service.update(
-        movie_genre_association_id,
-        data
-    )
 
 
 @router.delete("/{movie_genre_association_id}")
