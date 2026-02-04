c5b12d43bb1985148fb5979f634006f069de4428
spline <dmcemin@gmail.com>
2026-02-04 17:02:05 +0100

    add: MovieCacheInvalidatorDep

--- app/dependencies/cache.py
+++ app/dependencies/cache.py
@@ -5,6 +5,7 @@ from redis.asyncio.client import Redis
 
 from cache import redis_helper
 from cache.invalidator import CacheInvalidatorBase
+from cache.movie import MovieCacheInvalidator
 from schemas.cache import ModelCacheConfig
 
 RedisDep: TypeAlias = Annotated[Redis, Depends(redis_helper.get_client)]
@@ -26,12 +27,12 @@ def get_movie_person_cache_invalidator(cache: RedisDep) -> CacheInvalidatorBase:
     return CacheInvalidatorBase(cache, ModelCacheConfig(), "movie_person_associations")
 
 
-def get_movie_cache_invalidator(cache: RedisDep) -> CacheInvalidatorBase:
-    return CacheInvalidatorBase(cache, ModelCacheConfig(), "movies")
+def get_movie_cache_invalidator(cache: RedisDep) -> MovieCacheInvalidator:
+    return MovieCacheInvalidator(cache)
 
 
 CountryCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_countries_cache_invalidator)]
 GenreCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_genres_cache_invalidator)]
 PersonCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_persons_cache_invalidator)]
 MoviePersonCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_movie_person_cache_invalidator)]
-MovieCacheInvalidatorDep: TypeAlias = Annotated[CacheInvalidatorBase, Depends(get_movie_cache_invalidator)]
+MovieCacheInvalidatorDep: TypeAlias = Annotated[MovieCacheInvalidator, Depends(get_movie_cache_invalidator)]
