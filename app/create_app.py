cd021866134c4afa1f4ce30b1cfc03cc06c5888a
spline <dmcemin@gmail.com>
2026-02-06 21:13:34 +0100

    add: Elasticsearch to lifespan

--- app/create_app.py
+++ app/create_app.py
@@ -7,6 +7,7 @@ import event_handlers  # noqa: F401
 from cache import redis_helper
 from core import broker, fs_router
 from core.models import db_helper
+from elastic.elasticsearch import es_helper
 from exceptions.register import register_exception_handlers
 from storage.s3 import s3_helper
 
@@ -17,9 +18,12 @@ async def lifespan(app: FastAPI):
     if not broker.is_worker_process:
         await broker.startup()
     await redis_helper.connect()
+    await es_helper.connect()
+    await es_helper.documents_init()
 
     yield
 
+    await es_helper.close()
     await redis_helper.close()
     if not broker.is_worker_process:
         await broker.shutdown()
