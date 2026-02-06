e72cb8cca72ebe495b03e3abca416629e9ddbac2
spline <dmcemin@gmail.com>
2026-02-06 21:15:48 +0100

    add: register_elastic_exception_handlers to register_exception_handlers

--- app/exceptions/register.py
+++ app/exceptions/register.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from exceptions.db_handlers import register_db_exception_handlers
+from exceptions.elastic_handlers import register_elastic_exception_handlers
 from exceptions.redis_handlers import register_redis_exception_handlers
 from exceptions.s3_handlers import register_s3_exception_handlers
 
@@ -9,3 +10,4 @@ def register_exception_handlers(app: FastAPI) -> None:
     register_db_exception_handlers(app)
     register_s3_exception_handlers(app)
     register_redis_exception_handlers(app)
+    register_elastic_exception_handlers(app)
