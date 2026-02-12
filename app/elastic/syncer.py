3b439c66bb9b168d953fb5f020de883268bbae61
spline <dmcemin@gmail.com>
2026-02-12 20:41:36 +0100

    update: ElasticSyncer, add TElasticUpdateSchema, TElasticBulkUpdateSchema and bulk_update

--- app/elastic/syncer.py
+++ app/elastic/syncer.py
@@ -1,11 +1,14 @@
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
+from pydantic import BaseModel
 
 from schemas.base import Id
 
 
 class ElasticSyncer[
     TElasticSchema: Id,
+    TElasticUpdateSchema: BaseModel,
+    TElasticBulkUpdateSchema: Id,
 ]:
     def __init__(self, client: AsyncElasticsearch, index_name: str) -> None:
         self.client = client
@@ -16,7 +19,6 @@ class ElasticSyncer[
             index=self._index_name,
             id=str(data.id),
             document=data.model_dump(),
-            refresh=True
         )
 
     async def bulk_upsert(self, items: list[TElasticSchema]) -> None:
@@ -33,7 +35,28 @@ class ElasticSyncer[
             for data in items
         ]
 
-        await async_bulk(client=self.client, actions=actions, refresh=True)
+        await async_bulk(client=self.client, actions=actions)
+
+    async def update(self, document_id: int, data: TElasticUpdateSchema) -> None:
+        await self.client.update(
+            index=self._index_name,
+            id=str(document_id),
+            doc=data.model_dump(exclude_unset=True),
+            retry_on_conflict=3,
+        )
+
+    async def bulk_update(self, data: list[TElasticBulkUpdateSchema]) -> None:
+        actions = [
+            {
+                "_op_type": "update",
+                "_index": self._index_name,
+                "_id": str(item.id),
+                "retry_on_conflict": 3,
+                "doc": item.model_dump(exclude_unset=True),
+            }
+            for item in data
+        ]
+        await async_bulk(client=self.client, actions=actions)
 
     async def delete(self, document_id: int) -> None:
         await self.client.delete(
