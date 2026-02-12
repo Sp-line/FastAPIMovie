aa84345493d163182af89f932eb985ecfb492214
spline <dmcemin@gmail.com>
2026-02-12 22:55:34 +0100

    add: new methods to m2m repos

--- app/repositories/m2m.py
+++ app/repositories/m2m.py
@@ -1,3 +1,6 @@
+from typing import Sequence, Iterable
+
+from sqlalchemy import select, Row
 from sqlalchemy.exc import IntegrityError
 
 from core.models import MovieCountryAssociation, MovieGenreAssociation, MoviePersonAssociation
@@ -14,6 +17,7 @@ from signals.event_session import EventSession
 from signals.movie_country import movie_country_base_publishers
 from signals.movie_genre import movie_genre_base_publishers
 from signals.movie_person import movie_person_base_publishers
+from utils import populate_association_map
 
 
 class MovieCountryRepository(
@@ -34,6 +38,28 @@ class MovieCountryRepository(
             Eventer(movie_country_base_publishers),
             movie_country_event_schemas
         )
+
+    async def get_movie_ids_by_country_id(self, country_id: int) -> Sequence[int]:
+        stmt = select(MovieCountryAssociation.movie_id).where(MovieCountryAssociation.country_id == country_id)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_country_ids_by_movie_id(self, movie_id: int) -> Sequence[int]:
+        stmt = select(MovieCountryAssociation.country_id).where(MovieCountryAssociation.movie_id == movie_id)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_movie_ids_with_country_ids_by_movie_ids(self, movie_ids: Iterable[int]) -> dict[int, list[int]]:
+        if not movie_ids:
+            return {}
+        result_map: dict[int, list[int]] = {movie_id: [] for movie_id in movie_ids}
+
+        stmt = (
+            select(MovieCountryAssociation.movie_id, MovieCountryAssociation.country_id)
+            .where(MovieCountryAssociation.movie_id.in_(movie_ids))
+        )
+        result = await self._session.execute(stmt)
+        return populate_association_map(result.all(), result_map)
 
     def _handle_integrity_error(self, exc: IntegrityError) -> None:
         err_data = self._get_integrity_error_data(exc)
@@ -76,6 +102,29 @@ class MovieGenreRepository(
             movie_genre_event_schemas
         )
 
+    async def get_movie_ids_by_genre_id(self, genre_id: int) -> Sequence[int]:
+        stmt = select(MovieGenreAssociation.movie_id).where(MovieGenreAssociation.genre_id == genre_id)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_genre_ids_by_movie_id(self, movie_id: int) -> Sequence[int]:
+        stmt = select(MovieGenreAssociation.genre_id).where(MovieGenreAssociation.movie_id == movie_id)
+        result = await self._session.execute(stmt)
+        a = result.scalars().all()
+        return a
+
+    async def get_movie_ids_with_genre_ids_by_movie_ids(self, movie_ids: Iterable[int]) -> dict[int, list[int]]:
+        if not movie_ids:
+            return {}
+        result_map: dict[int, list[int]] = {movie_id: [] for movie_id in movie_ids}
+
+        stmt = (
+            select(MovieGenreAssociation.movie_id, MovieGenreAssociation.genre_id)
+            .where(MovieGenreAssociation.movie_id.in_(movie_ids))
+        )
+        result = await self._session.execute(stmt)
+        return populate_association_map(result.all(), result_map)
+
     def _handle_integrity_error(self, exc: IntegrityError) -> None:
         err_data = self._get_integrity_error_data(exc)
 
@@ -116,6 +165,28 @@ class MoviePersonRepository(
             Eventer(movie_person_base_publishers),
             movie_person_event_schemas
         )
+
+    async def get_movie_ids_by_person_id(self, person_id: int) -> Sequence[int]:
+        stmt = select(MoviePersonAssociation.movie_id).where(MoviePersonAssociation.person_id == person_id)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_person_ids_by_movie_id(self, movie_id: int) -> Sequence[int]:
+        stmt = select(MoviePersonAssociation.person_id).where(MoviePersonAssociation.movie_id == movie_id)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_movie_ids_with_person_ids_by_movie_ids(self, movie_ids: Iterable[int]) -> dict[int, list[int]]:
+        if not movie_ids:
+            return {}
+        result_map: dict[int, list[int]] = {movie_id: [] for movie_id in movie_ids}
+
+        stmt = (
+            select(MoviePersonAssociation.movie_id, MoviePersonAssociation.person_id)
+            .where(MoviePersonAssociation.movie_id.in_(movie_ids))
+        )
+        result = await self._session.execute(stmt)
+        return populate_association_map(result.all(), result_map)
 
     def _handle_integrity_error(self, exc: IntegrityError) -> None:
         err_data = self._get_integrity_error_data(exc)
