511b175355b3ed1a9c4a9b0a650af85ba7a512bb
spline <dmcemin@gmail.com>
2026-02-12 22:55:09 +0100

    add: populate_association_map utils import to utils.__init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,7 @@
 __all__ = (
     "camel_case_to_snake_case",
+    "populate_association_map"
 )
 
 from .case_converter import camel_case_to_snake_case
+from .populate_association_map import populate_association_map
