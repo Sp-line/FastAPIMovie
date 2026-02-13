2796ef350b93640b1414a00cb84b7a6775f55c52
spline <dmcemin@gmail.com>
2026-02-13 20:52:52 +0100

    add: JStream stream import to core.__init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ("broker", "fs_router")
+__all__ = ("broker", "fs_router", "stream")
 
 from .taskiq_broker import broker
-from .fs_router import router as fs_router
+from .fs_router import router as fs_router, stream
