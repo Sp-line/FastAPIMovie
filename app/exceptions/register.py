11a25417e47ce32bff1a19f1943d1212245bf3de
spline <dmcemin@gmail.com>
2026-02-14 13:59:23 +0100

    add: register UnsupportedMediaTypeException handler

--- app/exceptions/register.py
+++ app/exceptions/register.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from exceptions.db_handlers import register_db_exception_handlers
 from exceptions.elastic_handlers import register_elastic_exception_handlers
+from exceptions.files_handlers import register_files_exception_handlers
 from exceptions.redis_handlers import register_redis_exception_handlers
 from exceptions.s3_handlers import register_s3_exception_handlers
 
@@ -11,3 +12,4 @@ def register_exception_handlers(app: FastAPI) -> None:
     register_s3_exception_handlers(app)
     register_redis_exception_handlers(app)
     register_elastic_exception_handlers(app)
+    register_files_exception_handlers(app)
