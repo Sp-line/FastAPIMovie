572a9abab6017f1fbe9dc8bc5285a153b130d165
spline <dmcemin@gmail.com>
2026-02-14 14:00:16 +0100

    add: AllowedMimeTypes enum

--- app/constants/base.py
+++ app/constants/base.py
@@ -1,3 +1,23 @@
+from enum import StrEnum
+from functools import lru_cache
+
+
 class ImageUrlLimits:
     MAX: int = 1024
     MIN: int = 1
+
+
+class AllowedMimeTypes(StrEnum):
+    JPEG = "image/jpeg"
+    PNG = "image/png"
+    WEBP = "image/webp"
+
+    @classmethod
+    @lru_cache
+    def get_values(cls) -> list[str]:
+        return [member.value for member in cls]
+
+    @classmethod
+    @lru_cache
+    def get_error_string(cls) -> str:
+        return ", ".join(cls.get_values())
